006072831d93490f4233f86d821f46ad0e7a7e05
TanYiQing <yiqingtan99@gmail.com>
2022-02-20 23:26:09 +0800

    change layout

--- lib/pages/customer/notification/view/notification.dart
+++ lib/pages/customer/notification/view/notification.dart
@@ -3,19 +3,21 @@ import 'package:flutter/material.dart';
 class NotificationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-            backgroundColor: Colors.transparent,
-            title: Text(
-              "Notifications",
-              style: TextStyle(color: Colors.black),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+              backgroundColor: Colors.transparent,
+              title: Text(
+                "Notifications",
+                style: TextStyle(color: Colors.black),
+              ),
+              elevation: 0,
+              iconTheme: IconThemeData(color: Colors.black),
             ),
-            elevation: 0,
-            iconTheme: IconThemeData(color: Colors.black),
+        body: Center(
+          child: Container(
+            child: Text('Notification'),
           ),
-      body: Center(
-        child: Container(
-          child: Text('Notification'),
         ),
       ),
     );
