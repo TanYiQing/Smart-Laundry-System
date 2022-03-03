6af79e90bc51a3654cb720a9fe85d538b1f871a8
TanYiQing <yiqingtan99@gmail.com>
2022-03-03 22:32:21 +0800

    backend-servicesdetails

--- lib/pages/customer/servicesdetails/controller/servicesdetails_controller.dart
+++ lib/pages/customer/servicesdetails/controller/servicesdetails_controller.dart
@@ -1,5 +1,19 @@
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
 class ServicesDetailsController extends GetxController {
   var laundry = Get.arguments;
+  late ScrollController scrollcontroller;
+  double scrollcontrolleroffset = 0.0;
+
+  @override
+  void onInit() {
+    scrollcontroller=ScrollController();
+    scrollcontroller.addListener(scrollListener);
+    super.onInit();
+  }
+  scrollListener(){
+    scrollcontrolleroffset=scrollcontroller.offset;
+    update();
+  }
 }
