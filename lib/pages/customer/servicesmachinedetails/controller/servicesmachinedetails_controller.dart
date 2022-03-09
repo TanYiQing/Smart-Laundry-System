b0d64866d4070d93b6ae4f805e6e1ef5c3e10f89
TanYiQing <yiqingtan99@gmail.com>
2022-03-09 22:28:11 +0800

    backend-servicesmachinedetails

--- lib/pages/customer/servicesmachinedetails/controller/servicesmachinedetails_controller.dart
+++ lib/pages/customer/servicesmachinedetails/controller/servicesmachinedetails_controller.dart
@@ -2,4 +2,10 @@ import 'package:get/get.dart';
 
 class ServicesMachineDetailsController extends GetxController {
   var machine = Get.arguments;
+
+  @override
+  void onInit() {
+    print(machine.addOn1);
+    super.onInit();
+  }
 }
