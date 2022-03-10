6c67e93482b9e66416adda58c821e78972edeb6e
TanYiQing <yiqingtan99@gmail.com>
2022-03-10 15:58:32 +0800

    backend-servicesmachinedetails (radio button)

--- lib/pages/customer/servicesmachinedetails/controller/servicesmachinedetails_controller.dart
+++ lib/pages/customer/servicesmachinedetails/controller/servicesmachinedetails_controller.dart
@@ -1,7 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
 class ServicesMachineDetailsController extends GetxController {
   var machine = Get.arguments;
+  TextEditingController notecontroller = new TextEditingController();
+  var orderMethod = "".obs;
 
   @override
   void onInit() {
