84e64a51d6b95736f33a00ab2603b2b72574ae69
TanYiQing <yiqingtan99@gmail.com>
2022-03-14 16:27:15 +0800

    backend-servicesmachinedeatils

--- lib/pages/customer/servicesmachinedetails/controller/servicesmachinedetails_controller.dart
+++ lib/pages/customer/servicesmachinedetails/controller/servicesmachinedetails_controller.dart
@@ -8,15 +8,22 @@ class ServicesMachineDetailsController extends GetxController {
   var checked1 = false.obs;
   var checked2 = false.obs;
   var checked3 = false.obs;
+  var totalPrice = 0.00.obs;
 
   @override
   void onInit() {
+    totalPrice.value = double.parse(machine.price);
     print(machine.addOn1);
     super.onInit();
   }
 
   void handleRadioButton(var method) {
     orderMethod.value = method;
+    if (orderMethod.value == "Reservation") {
+      totalPrice.value += 0;
+    } else {
+      totalPrice.value += 5;
+    }
     update();
     print(orderMethod.value);
   }
@@ -24,13 +31,20 @@ class ServicesMachineDetailsController extends GetxController {
   void handleCheckBox(var checkValue, String addon) {
     if (addon == "addon1") {
       checked1.value = checkValue;
+      if (checked1.value == true) {
+        totalPrice.value += double.parse(machine.addOn1Price);
+      }
       print(checked1.value);
     } else if (addon == "addon2") {
       checked2.value = checkValue;
-      print(checked2.value);
+      if (checked1.value == true) {
+        totalPrice.value += double.parse(machine.addOn2Price);
+      }
     } else {
       checked3.value = checkValue;
-      print(checked3.value);
+      if (checked1.value == true) {
+        totalPrice.value += double.parse(machine.addOn3Price);
+      }
     }
 
     update();
