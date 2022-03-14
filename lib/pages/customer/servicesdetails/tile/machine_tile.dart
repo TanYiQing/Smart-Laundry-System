06bc6d5e853c2ea02b5642bf8917d1c976db55a4
TanYiQing <yiqingtan99@gmail.com>
2022-03-14 21:41:30 +0800

    backend-machinetile

--- lib/pages/customer/servicesdetails/tile/machine_tile.dart
+++ lib/pages/customer/servicesdetails/tile/machine_tile.dart
@@ -10,72 +10,63 @@ class MachineTile extends StatelessWidget {
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
 
-    return Stack(
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-                height: screenWidth / 3.5,
-                width: double.infinity,
-                child: Image.asset((machine.machineType == "Washing Machine")
-                    ? "assets/images/washingmachine.png"
-                    : (machine.machineType == "Dry Washing Machine")
-                        ? "assets/images/drywashingmachine.png"
-                        : "assets/images/ironingmachine.png")),
-            Text(
-              machine.machineType.toString(),
-              style: TextStyle(fontSize: screenWidth / 23),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 4.0),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        Container(
+            height: screenWidth / 3.5,
+            width: double.infinity,
+            child: Image.asset((machine.machineType == "Washing Machine")
+                ? "assets/images/washingmachine.png"
+                : (machine.machineType == "Dry Washing Machine")
+                    ? "assets/images/drywashingmachine.png"
+                    : "assets/images/ironingmachine.png")),
+        Text(
+          machine.machineType.toString(),
+          style: TextStyle(fontSize: screenWidth / 23),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 4.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text("max." + machine.maximumWeight.toString() + "kg",
-                          style: TextStyle(fontSize: screenWidth / 40)),
-                      Text("min." + machine.minimumWeight.toString() + "kg",
-                          style: TextStyle(fontSize: screenWidth / 40)),
-                    ],
+                  Text("max." + machine.maximumWeight.toString() + "kg",
+                      style: TextStyle(fontSize: screenWidth / 40)),
+                  Text("min." + machine.minimumWeight.toString() + "kg",
+                      style: TextStyle(fontSize: screenWidth / 40)),
+                ],
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(
+                    "RM" + machine.price.toString(),
+                    style: TextStyle(
+                        fontSize: screenWidth / 23,
+                        fontWeight: FontWeight.bold),
                   ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Text(
-                        "RM" + machine.price.toString(),
-                        style: TextStyle(
-                            fontSize: screenWidth / 23,
-                            fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        machine.calculationBase.toString(),
-                        style: TextStyle(fontSize: screenWidth / 35),
-                      ),
-                    ],
+                  Text(
+                    machine.calculationBase.toString(),
+                    style: TextStyle(fontSize: screenWidth / 35),
                   ),
                 ],
               ),
-            ),
-            Text(
-              machine.available.toString(),
-              style: TextStyle(
-                  fontSize: screenWidth / 23,
-                  fontWeight: FontWeight.bold,
-                  color: (machine.available == "Available")
-                      ? Colors.green
-                      : Colors.red),
-            ),
-          ],
+            ],
+          ),
         ),
-        Container(
-          color: (machine.available == "Not Available")
-              ? Color.fromRGBO(255, 255, 255, 0.7)
-              : Colors.transparent,
-        )
+        Text(
+          (machine.available == "Available") ? "Available" : "Occupied Now",
+          style: TextStyle(
+              fontSize: screenWidth / 23,
+              fontWeight: FontWeight.bold,
+              color: (machine.available == "Available")
+                  ? Colors.green
+                  : Colors.red),
+        ),
       ],
     );
   }
