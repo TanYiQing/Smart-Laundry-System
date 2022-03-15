cb23ce686812d241f54f5831b16d189c43aa7608
TanYiQing <yiqingtan99@gmail.com>
2022-03-15 22:48:56 +0800

    frontend-location

--- lib/pages/customer/location/view/location.dart
+++ lib/pages/customer/location/view/location.dart
@@ -3,16 +3,26 @@ import 'package:flutter/material.dart';
 class LocationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        title: Text(
-          "Location",
-          style: TextStyle(color: Colors.black),
-        ),
-        elevation: 0,
-        iconTheme: IconThemeData(color: Colors.black),
-      ),
+          backgroundColor: Colors.transparent,
+          title: Text(
+            "Location",
+            style: TextStyle(color: Colors.black),
+          ),
+          elevation: 0,
+          iconTheme: IconThemeData(color: Colors.black),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset(
+                "assets/icons/add-point.png",
+                width: screenWidth / 15,
+              ),
+            )
+          ]),
       body: Center(
         child: Container(
           child: Text('Location'),
